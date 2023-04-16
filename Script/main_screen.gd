408a7c35abaf58cbc33ea3775557c3debca988ab
frisk038 <frisktrentehuit@gmail.com>
2023-04-16 19:10:02 +0200

    first ios build

--- Script/main_screen.gd
+++ Script/main_screen.gd
@@ -4,8 +4,7 @@ extends ColorRect
 func _ready():
 	pass
 
-
 func _on_TextureButton_pressed():
-	var err = get_tree().change_scene("res://main.tscn")
+	var err = get_tree().change_scene("res://Scene/Main.tscn")
 	if err != OK :
 		print("cant load game scene")
